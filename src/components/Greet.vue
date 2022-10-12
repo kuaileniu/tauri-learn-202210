88b7e77fa2d7a2f78c42f55b909ec4ef18d2dace
jtfeng <jiangtao.feng@baoying.com>
2022-10-12 17:12:55 +0800

    add breakdown config for debug

--- src/components/Greet.vue
+++ src/components/Greet.vue
@@ -6,6 +6,7 @@ const greetMsg = ref("");
 const name = ref("");
 
 async function greet() {
+  console.log("greet.......")
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
   greetMsg.value = await invoke("greet", { name: name.value });
 }
@@ -14,7 +15,7 @@ async function greet() {
 <template>
   <div class="card">
     <input id="greet-input" v-model="name" placeholder="Enter a name..." />
-    <button type="button" @click="greet()">Greet</button>
+    <button type="button" @click="greet()">Greet2</button>
   </div>
 
   <p>{{ greetMsg }}</p>

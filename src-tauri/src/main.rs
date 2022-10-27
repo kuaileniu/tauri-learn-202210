475a58ea89fe5f9a8c76a5b97bc8e7651ae97de6
jtfeng <jiangtao.feng@baoying.com>
2022-10-27 15:50:04 +0800

    添加含入惨及出参的demo

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,9 +10,16 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn args_command(a: f32, b: f32) -> f32 {
+    println!("I was invoked from JS, with this message: {} {}", a, b);
+    let c = a * b;
+    c
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, args_command])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ src/components/InOut.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import { invoke } from "@tauri-apps/api/tauri";
+defineProps<{ msg: String }>();
+const count = ref(0);
+const commandMsg = ref("");
+onMounted(() => {
+  const a = 0.07;
+  const b = 100;
+  let c = a * b;
+  console.log("ts计算结果：" + c); //ts计算结果：7.000000000000001
+});
+
+// 需 args_command 的驼峰方式命名
+const argsCommand = async () => {
+  const a = 0.07;
+  const b = 100;
+  //   let c = await invoke("args_command", { a: a, b: b });
+  let c = await invoke("args_command", { a, b });
+  console.log("Rust 返回的值为: " + c);
+  commandMsg.value = c;
+};
+</script>
+<template>
+  <button @click="argsCommand">有进参和回参数方式调用Rust</button>
+
+  <p>Rust 计算结果为: {{ commandMsg }}</p>
+</template>
+<style scoped>
+</style>

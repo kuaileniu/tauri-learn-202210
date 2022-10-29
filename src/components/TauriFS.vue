7e9a2129750bf9bff88324a2f08398a5e9092421
jtfeng <jiangtao.feng@baoying.com>
2022-10-29 23:12:38 +0800

    asset方式读取图片

--- src/components/TauriFS.vue
+++ src/components/TauriFS.vue
@@ -1,20 +1,31 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import { readBinaryFile, BaseDirectory } from "@tauri-apps/api/fs";
-// Copy the `$APPDIR/app.conf` file to `$APPDIR/app.conf.bk`
-// await copyFile('app.conf', 'app.conf.bk', { dir: BaseDirectory.App });
+import { resourceDir, join } from "@tauri-apps/api/path";
+import { convertFileSrc } from "@tauri-apps/api/tauri";
+
+const imgSrc = ref();
 
 const readImg = async () => {
-  // Read the image file in the `$RESOURCEDIR/avatar.png` path
   const contents = await readBinaryFile("img/avatar.png", {
     dir: BaseDirectory.Resource,
   });
-  console.log(contents)
+  console.log(contents);
+};
+
+const readImgTwo = async () => {
+  const resourceDirPath = await resourceDir();
+  const filePath = await join(resourceDirPath, "img/avatar.png");
+  const assetUrl = convertFileSrc(filePath);
+  console.log(`resourceDirPath:${resourceDirPath} ,\n filePath:${filePath} \n ,assetUrl:${assetUrl}`)
+  imgSrc.value = assetUrl;
 };
 </script>
 
 <template>
-    <button @click="readImg">读取文件指定路径下的文件(前端TS)</button>
+  <button @click="readImg">读取文件指定路径下的文件(前端TS)</button>
+  <button @click="readImgTwo">通过路径直接显示文件（前端TS）</button>
+  <img :src="imgSrc" alt="测试图片img/avatar.png"/>
 </template>
 <style scoped>
-
 </style>

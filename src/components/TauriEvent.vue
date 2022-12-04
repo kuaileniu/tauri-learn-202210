dd2e0dc789a11068ebe082d8f4a495b32484bd2d
Happy bull <hggfjt@126.com>
2022-12-04 10:34:43 +0800

    Update TauriEvent.vue

--- src/components/TauriEvent.vue
+++ src/components/TauriEvent.vue
@@ -12,12 +12,19 @@ const initProcess = async () => {
   await invoke("init_process");
   console.log("通知rust端(只通知一次)-------------------");
    // 自动转成TS的类型
-  await listen<EventPaload>("event-name-hggfjt", (event) => {
+  await listen<EventPaload>("event-name-hggfjt", (event) => { 
     const pay = event.payload;
     console.log(pay);
     console.log(event);
   });
 };
+
+//  独立执行
+//  await listen<EventPaload>("event-name-hggfjt", (event) => { 
+//    const pay = event.payload;
+//    console.log(pay);
+//    console.log(event);
+//  });
 </script>
 <template>
   <button @click="initProcess">触发事件(rust给ts推送对象信息)</button>
